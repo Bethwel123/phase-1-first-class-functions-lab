ed597bc77aee6b174fe6cb50e2314e214c1f5292
Bethwel123 <bethwellangat654@gmail.com>
2024-06-26 05:00:40 +0300

    Done first-class functions

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0, 2);
+}
+
+function returnLastTwoDrivers(drivers) {
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+function createFareMultiplier(multiplier) {
+    return function (value) {
+      return value * multiplier;
+    };
+  }
+
+function fareDoubler(double) {
+    return double * 2;
+}
+
+function fareTripler(triple) {
+    return triple * 3;
+}
+
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0, 2);
+}
+  
+function returnLastTwoDrivers(drivers) {
+    return drivers.slice(-2);
+}
+  
+function selectDifferentDrivers(drivers, driverSelector) {
+    return driverSelector(drivers);}
+  
